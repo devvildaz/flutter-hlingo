bfb8f51b8041ea2ad63717037e0ed65a4b5edba6
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-06-04 11:41:03 -0500

    fixes: fix route

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'HoloLingo',
-        initialRoute: '/search',
+        initialRoute: '/',
         routes: getRoutes());
   }
 }
