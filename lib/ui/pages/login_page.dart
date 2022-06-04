c453a4d8c6c48b0a7d4282f636b07f18b59a720d
unknown <erickp1502@gmail.com>
2022-06-04 10:53:25 -0500

    fix: login register pages

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget{
-  const Login({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login',
-      home: LoginPage(),
-    );
-  }
-}
-
 class LoginPage extends StatefulWidget{
   // const LoginPage({Key? key}) : super(key: key);
 
