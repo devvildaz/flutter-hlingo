bd0e90e0e8be4022b07a4bb8c77027710470c1b5
Edwin Aguilar <edwinaguilar1a@gmail.com>
2022-06-04 11:24:53 -0500

    form de login corregido

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -22,53 +22,56 @@ class _LoginPage extends State<LoginPage>{
       body: Container(
         margin: const EdgeInsets.only(left: 100,right: 100),
         alignment: Alignment.center,
-        child: Column(
-          children: <Widget>[
-            const SizedBox(height: 50,),
-            Image.asset('assets/main_logo.png'),
-            const SizedBox(height: 50,),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Correo Electrónico'
-              ),
-              validator: (value) {
-                if (!_isEmail(value.toString())){
-                  return 'Ingrese un correo electrónico válido';
-                }
-              },
-            ),
-            const SizedBox(height: 50,),
-            TextFormField(
-              decoration: const InputDecoration(
-                  labelText: 'Contraseña'
-              ),
-              validator: (value) {
-                if (value!.isEmpty){
-                  return 'Ingrese una contraseña válida';
-                }
-              },
-            ),
-            const SizedBox(height: 50,),
-            OutlinedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.indigo[600]),
-                  foregroundColor: MaterialStateProperty.all<Color>(const Color(0xffffffff),),
-                  textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(
-                      color:Colors.black,
-                      fontFamily: 'Arial')),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 50,),
+              Image.asset('assets/main_logo.png'),
+              const SizedBox(height: 50,),
+              TextFormField(
+                decoration: const InputDecoration(
+                    labelText: 'Correo Electrónico'
                 ),
-                onPressed: (){
-                  if(_formKey.currentState!.validate()) {
-                    Scaffold.of(context).showSnackBar(
-                        const SnackBar(content:Text("Accesando al Sistema"))
-                    );
+                validator: (value) {
+                  if (!_isEmail(value.toString())){
+                    return 'Ingrese un correo electrónico válido';
                   }
                 },
-                child: Text('Iniciar Sesión')),
-          ],
-        ),
+              ),
+              const SizedBox(height: 50,),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                    labelText: 'Contraseña'
+                ),
+                validator: (value) {
+                  if (value!.isEmpty){
+                    return 'Ingrese una contraseña válida';
+                  }
+                },
+              ),
+              const SizedBox(height: 50,),
+              OutlinedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.indigo[600]),
+                    foregroundColor: MaterialStateProperty.all<Color>(const Color(0xffffffff),),
+                    textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(
+                        color:Colors.black,
+                        fontFamily: 'Arial')),
+                  ),
+                  onPressed: (){
+                    if(_formKey.currentState!.validate()) {
+                      Scaffold.of(context).showSnackBar(
+                          const SnackBar(content:Text("Accesando al Sistema"))
+                      );
+                    }
+                  },
+                  child: Text('Iniciar Sesión')),
+            ],
+          ),
+        )
       ),
-
     );
   }
 
