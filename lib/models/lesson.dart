6ee18c77364aecbc4345f8f7058869355a58db9d
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-06-25 11:39:04 -0500

    refactor: extract models in a new folder

---
+++ lib/models/lesson.dart
@@ -0,0 +1,77 @@
+// To parse this JSON data, do
+//
+//     final Lesson = LessonFromJson(jsonString);
+
+import 'dart:convert';
+
+Lesson lessonFromJson(String str) => Lesson.fromJson(json.decode(str));
+
+String lessonToJson(Lesson data) => json.encode(data.toJson());
+
+class Lesson {
+  Lesson({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.exampleVideo,
+    required this.categoryName,
+    this.dateCreated,
+  });
+
+  Id id;
+  String title;
+  String description;
+  String exampleVideo;
+  String categoryName;
+  DateCreated? dateCreated;
+
+  factory Lesson.fromJson(Map<String, dynamic> json) => Lesson(
+        id: Id.fromJson(json["_id"]),
+        title: json["title"],
+        description: json["description"],
+        exampleVideo: json["example_video"],
+        categoryName: json["category_name"],
+        dateCreated: DateCreated.fromJson(json["dateCreated"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id.toJson(),
+        "title": title,
+        "description": description,
+        "example_video": exampleVideo,
+        "category_name": categoryName,
+        "dateCreated": dateCreated?.toJson(),
+      };
+}
+
+class DateCreated {
+  DateCreated({
+    this.date,
+  });
+
+  int? date;
+
+  factory DateCreated.fromJson(Map<String, dynamic> json) => DateCreated(
+        date: json["\u0024date"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "\u0024date": date,
+      };
+}
+
+class Id {
+  Id({
+    required this.oid,
+  });
+
+  String oid;
+
+  factory Id.fromJson(Map<String, dynamic> json) => Id(
+        oid: json["\u0024oid"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "\u0024oid": oid,
+      };
+}
