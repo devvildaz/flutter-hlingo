f9fc1de779c3709e8528f5b8e84446f73a07afac
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-07-02 02:02:15 -0500

    feat: lesson cards list view

--- lib/ui/widgets/sections_list.dart
+++ lib/ui/widgets/sections_list.dart
@@ -26,11 +26,17 @@ class SectionsList extends StatefulWidget {
 
 class _SectionsListState extends State<SectionsList> {
   final lessonsProvider = LessonsProvider();
-  late List<Category> sections;
+  List<Category> _sections = [];
 
   @override
   void initState() {
     super.initState();
+    final lessons = LessonsProvider().getLessons();
+    lessons.then((lessons) {
+      setState(() {
+        _sections = _createSections(lessons);
+      });
+    });
   }
 
   @override
@@ -40,40 +46,40 @@ class _SectionsListState extends State<SectionsList> {
     );
   }
 
+  List<Category> _createSections(List<Lesson> lessons) {
+    final categories =
+        lessons.map((lesson) => lesson.categoryName).toSet().toList();
+
+    //group lessons by category in a list of Category objects
+    final sections = categories.map((category) {
+      final lessonsInCategory =
+          lessons.where((lesson) => lesson.categoryName == category).toList();
+      return Category(
+        name: category,
+        expandedValue: lessonsInCategory.map((lesson) {
+          return LessonCard(
+            title: lesson.title,
+          );
+        }).toList(),
+      );
+    }).toList();
+    return sections;
+  }
+
   Widget _buildPanel() {
     return FutureBuilder<List<Lesson>>(
       future: lessonsProvider.getLessons(),
       builder: (BuildContext context, AsyncSnapshot<List<Lesson>> snapshot) {
         if (snapshot.hasData) {
-          List<Lesson> lessons = snapshot.data!;
-
-          final categories =
-              lessons.map((lesson) => lesson.categoryName).toSet().toList();
-
-          //group lessons by category in a list of Category objects
-          sections = categories.map((category) {
-            final lessons_in_category = lessons
-                .where((lesson) => lesson.categoryName == category)
-                .toList();
-            return Category(
-              name: category,
-              expandedValue: lessons_in_category.map((lesson) {
-                return LessonCard(
-                  title: lesson.title,
-                );
-              }).toList(),
-            );
-          }).toList();
-
           return ExpansionPanelList(
             expandedHeaderPadding: const EdgeInsets.symmetric(vertical: 8),
             elevation: 0,
             expansionCallback: (int index, bool isExpanded) {
               setState(() {
-                sections[index].isExpanded = !isExpanded;
+                _sections[index].isExpanded = !isExpanded;
               });
             },
-            children: sections.map((section) {
+            children: _sections.map<ExpansionPanel>((section) {
               return ExpansionPanel(
                   canTapOnHeader: true,
                   backgroundColor: Colors.grey[100],
@@ -87,7 +93,17 @@ class _SectionsListState extends State<SectionsList> {
                       ),
                     );
                   },
-                  body: Text("a"),
+                  body: Container(
+                    height: 130,
+                    color: Colors.white,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: section.expandedValue.length,
+                      itemBuilder: (context, index) {
+                        return section.expandedValue[index];
+                      },
+                    ),
+                  ),
                   isExpanded: section.isExpanded);
             }).toList(),
           );
@@ -98,37 +114,5 @@ class _SectionsListState extends State<SectionsList> {
         }
       },
     );
-
-    // return ExpansionPanelList(
-    //   expandedHeaderPadding: const EdgeInsets.symmetric(vertical: 8),
-    //   elevation: 0,
-    //   expansionCallback: (int index, bool isExpanded) {
-    //     setState(() {
-    //       _data[index].isExpanded = !isExpanded;
-    //     });
-    //   },
-    //   children: _data.map<ExpansionPanel>((Item item) {
-    //     return ExpansionPanel(
-    //       canTapOnHeader: true,
-    //       backgroundColor: Colors.grey[100],
-    //       headerBuilder: (BuildContext context, bool isExpanded) {
-    //         return Container(
-    //           alignment: Alignment.centerLeft,
-    //           padding: const EdgeInsets.symmetric(horizontal: 12),
-    //           child: Text(
-    //             item.lesson.title,
-    //             style:
-    //                 const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-    //           ),
-    //         );
-    //       },
-    //       body: Container(
-    //           child: item.expandedValue,
-    //           color: Colors.white,
-    //           alignment: Alignment.center),
-    //       isExpanded: item.isExpanded,
-    //     );
-    //   }).toList(),
-    // );
   }
 }
